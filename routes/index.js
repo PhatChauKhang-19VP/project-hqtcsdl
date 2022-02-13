ab153f9fd3d4c8f2fb62d3e0c8fb7542e6ebacef
Ngo Minh Phat <19126008@student.hcmus.edu.vn>
2022-02-13 14:34:44 +0700

    Connect to sql server on cloud

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,21 @@
 var express = require('express');
 var router = express.Router();
 
+const sql = require('mssql/msnodesqlv8');
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+	res.render('index', { title: 'Express' });
 });
 
+router.post('/query', async (req, res, next) => {
+	const query = req.body.query;
+	console.log(req.body);
+	try {
+		const result = await sql.query(query);
+		res.json(result);
+	} catch (err) {
+		res.json({ error: err.message });
+	}
+});
 module.exports = router;
